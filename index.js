fdd08bf7fc05ce72c7e75765d9c9194b2e71a02e
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-08-22 22:50:05 +0400

    added full javascript

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 const result = document.querySelector('span');
-
-if (window.navigator.onLine) {
-  setOnline();
-} else {
-  setOffline();
-}
-
-window.addEventListener('offline', () => {
-  setOffline();
-});
-
-
-window.addEventListener('online', () => {
-  setOnline();
-});
-
-function setOnline() {
-  result.innerHTML = 'online';
-}
+const btn = document.querySelector('#btn');
+const text = document.querySelector('#text');
 
 function setOffline() {
   result.innerHTML = 'offline';
+  result.style.color = 'red';
+}
+
+function setOnline() {
+  result.innerHTML = 'online';
+  result.style.color = 'green';
 }
 
 function reload() {
   window.location.reload();
 }
+
+
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+    text.textContent = 'Hello World';
+
+    if (window.navigator.onLine) {
+      setOnline();
+    } else {
+      setOffline();
+    }
+
+    window.addEventListener('offline', () => {
+      setOffline();
+    });
+
+    window.addEventListener('online', () => {
+      setOnline();
+    });
+
+  }, 1000);
+});
