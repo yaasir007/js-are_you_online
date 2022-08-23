84f85845dc449fe7100cd6e8ca43f533db5c1743
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-08-23 23:42:59 +0400

    added js and css

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ function reload() {
 
 
 btn.addEventListener('click', () => {
+
+  let text = document.querySelector('#text');
+  let test = 'fuck you bitch!!!';
+
+
   setTimeout(() => {
     if (window.navigator.onLine) {
       setOnline();
@@ -32,5 +37,55 @@ btn.addEventListener('click', () => {
     window.addEventListener('online', () => {
       setOnline();
     });
+
+    let result  = test.repeat(1);
+    text.textContent = result;
+
   }, 1000);
+
+  setTimeout(() => {
+    let result  = test.repeat(2);
+    text.textContent = result;
+  }, 2000);
+
+  setTimeout(() => {
+    let result  = test.repeat(3);
+    text.textContent = result;
+  }, 3000);
+
+  setTimeout(() => {
+    let result  = test.repeat(4);
+    text.textContent = result;
+  }, 4000);
+
+  setTimeout(() => {
+    let result  = test.repeat(5);
+    text.textContent = result;
+  }, 5000);
+
+  setTimeout(() => {
+    let result  = test.repeat(6);
+    text.textContent = result;
+  }, 6000);
+
+  setTimeout(() => {
+    let result  = test.repeat(7);
+    text.textContent = result;
+  }, 7000);
+
+  setTimeout(() => {
+    let result  = test.repeat(8);
+    text.textContent = result;
+  }, 8000);
+
+  setTimeout(() => {
+    let result  = test.repeat(9);
+    text.textContent = result;
+  }, 9000);
+
+  setTimeout(() => {
+    let result  = test.repeat(10);
+    text.textContent = result;
+  }, 10000);
+
 });
